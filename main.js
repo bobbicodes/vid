3be781c9c3760d77c5fe37accec28d58a8a64bac
Bobby Towers <porkostomus@mail.com>
2022-03-21 15:19:43 -0700

    html5 video player

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          }
     })
-  
+
     win.loadFile('index.html')
-  }
+}
 
   app.whenReady().then(() => {
     createWindow()
